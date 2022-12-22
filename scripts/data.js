ca376f79c479f9690b8bd63d0c873b41b21acfa6
cwjinhan <91926462+cwjinhan@users.noreply.github.com>
2022-12-22 17:10:43 +0900

    updated

--- scripts/data.js
+++ scripts/data.js
@@ -45,7 +45,7 @@ function getProductByName(cw) {
 			id: '1',
 			name: 'iPhone',
 			descript: 'iPhone is...'
-/*			images: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-1-202209?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1660753619946"*/
+/*			images: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-1-202209?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1660753619946'*/
 		},
 		{
 			id: '2',
